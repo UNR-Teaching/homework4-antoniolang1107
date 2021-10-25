c9e1198281dcddeca7894c017fa05332ae3b7b6d
Tony Lang <TonylsLame@users.noreply.github.com>
2021-10-24 22:22:03 -0700

    Started

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <random>
+#include <iostream>
+#include <time.h>
+#include <fstream>
+
+void bubbleSort(int arr[], int size);
+void selectionSort(int arr[], int size);
+void mergeSort(int arr[]);
+
+
+int main() {
+    clock_t start = clock();
+
+    static const int SIZE = 1000, RAND_RANGE = 100000;
+    int dataArray[SIZE];
+    for (int i = 0; i < sizeof(dataArray)/ sizeof(dataArray[0]); i++) {
+        //dataArray[i] = rand % RAND_RANGE;
+    }
+    
+    int test[] = {4, 12, 6, 50, 2, 1, 90, 40, 17, 4};
+    selectionSort(test, sizeof(test) / sizeof(test[0]));
+
+    for (int i = 0; i < sizeof(test)/sizeof(test[0]); i++) {
+        std::cout << "Index " << i << ": " << test[i] << std::endl;
+        }
+
+    return 0;
+}
+
+void bubbleSort(int arr[], int size) {
+    int tempVal;
+    int numSwap = 0, numCompare = 0;
+
+    for (int i = 0; i < size - 1; i++) {
+
+        for (int j = i+1; j < size; j++) {
+            if (arr[j] < arr[i]) {
+                tempVal = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tempVal;
+
+                numSwap++;
+                numCompare++;
+            }
+        }
+    }
+}
+
+void selectionSort(int arr[], int size) {
+    int minIndex, j, i;
+    int tempInt;
+    int numSwap = 0, numCompare = 0;
+
+    for (int i = 0; i < size - 1; i++) {
+
+        minIndex = i;
+        for (int j = i + 1; j < size-1; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+                
+                numCompare++;
+            }
+        }
+        tempInt = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = tempInt;
+
+        numSwap++;
+    }
+}
+
+
+void mergeSort(int arr[], int left, int mid, int right) {
+
+}
